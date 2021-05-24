94fe32ea49d68f679d848db9d8f570920bd36204
Lopes <klinsmann.lopes@somapay.com.br>
2021-05-24 16:52:34 -0300

    Implementation of conventions in other currencies

---
+++ src/main/java/com/br/currencyConverter/services/ConverterEUR.java
@@ -0,0 +1,60 @@
+package com.br.currencyConverter.services;
+
+import com.br.currencyConverter.apis.exchangerate.ExchangeRatesApi;
+import com.br.currencyConverter.dtos.inputs.TransactionInputDTO;
+import com.br.currencyConverter.dtos.outputs.RateDTO;
+import com.br.currencyConverter.enums.Currencies;
+import com.br.currencyConverter.exceptions.BusinessRuleException;
+import com.br.currencyConverter.interfaces.ConverterInterface;
+import com.br.currencyConverter.models.transactions.Transactions;
+import com.br.currencyConverter.repositories.TransactionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.sql.Timestamp;
+
+@Service
+public class ConverterEUR implements ConverterInterface {
+
+    @Autowired
+    private ExchangeRatesApi exchangeRatesApi;
+
+    @Autowired
+    private TransactionsRepository transactionsRepository;
+
+    @Override
+    public boolean responsible(Currencies type) {
+        return type == Currencies.EUR;
+    }
+
+    @Override
+    public RateDTO getListRates() throws BusinessRuleException {
+        RateDTO rateDTO = exchangeRatesApi.getListRates();
+        return rateDTO;
+    }
+
+    @Override
+    public double converter(double rate, double originValue) {
+        return rate * originValue;
+    }
+
+    @Override
+    public Mono<Transactions> converterAndSave(TransactionInputDTO transactionInputDTO) throws BusinessRuleException {
+        RateDTO rateDTO = getListRates();
+        double rateUsed = rateDTO.getRates().getEUR();
+        double destinyValue = converter(rateUsed, transactionInputDTO.getOriginValue());
+
+        Transactions transaction = new Transactions(
+                transactionInputDTO.getIdUser(),
+                transactionInputDTO.getOriginCurrency(),
+                transactionInputDTO.getOriginValue(),
+                transactionInputDTO.getDestinyCurrency(),
+                destinyValue,
+                rateUsed,
+                new Timestamp(System.currentTimeMillis())
+        );
+
+        return transactionsRepository.save(transaction);
+    }
+}
