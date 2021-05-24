f81f93ec0bb18dcb41444cee66a96efe5cfa0141
Lopes <klinsmann.lopes@somapay.com.br>
2021-05-24 20:06:23 -0300

    Transaction listing service

--- src/main/java/com/br/currencyConverter/repositories/TransactionsRepository.java
+++ src/main/java/com/br/currencyConverter/repositories/TransactionsRepository.java
@@ -3,7 +3,10 @@ package com.br.currencyConverter.repositories;
 
 import com.br.currencyConverter.models.transactions.Transactions;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface TransactionsRepository extends ReactiveCrudRepository<Transactions,Integer> {
+
+    Flux<Transactions> findAllByUserCode(String userCode);
 
 }
--- src/main/java/com/br/currencyConverter/services/ExchangeRatesService.java
+++ src/main/java/com/br/currencyConverter/services/ExchangeRatesService.java
@@ -5,10 +5,12 @@ import com.br.currencyConverter.exceptions.BusinessRuleException;
 import com.br.currencyConverter.factory.ConverterFactory;
 import com.br.currencyConverter.interfaces.ConverterInterface;
 import com.br.currencyConverter.models.transactions.Transactions;
+import com.br.currencyConverter.repositories.TransactionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.management.InstanceNotFoundException;
@@ -19,6 +21,9 @@ public class ExchangeRatesService {
     @Autowired
     private ConverterFactory converterFactory;
 
+    @Autowired
+    private TransactionsRepository transactionsRepository;
+
     public Mono<Transactions> converter(TransactionInputDTO transactionInputDTO) throws BusinessRuleException, InstanceNotFoundException {
         ConverterInterface converterInterface = converterFactory.getInstance(transactionInputDTO.getDestinyCurrency());
         Mono<Transactions> transaction = converterInterface.converterAndSave(transactionInputDTO);
@@ -26,11 +31,19 @@ public class ExchangeRatesService {
         return transaction.switchIfEmpty(
                 monoResponseStatusBadRequest()
         );
+    }
 
+    public Flux<Transactions> findTransactionsByUser(String user) {
+        return transactionsRepository.findAllByUserCode(user)
+                .switchIfEmpty(monoResponseStatusNotFoundException());
     }
 
     public <T> Mono<T> monoResponseStatusBadRequest() {
         return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ocorreu algum erro ao tentar salvar a transação"));
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "O usuário não possui nenhuma transação"));
     }
 
 }
