b9a562b1d6f16cf401a87d177f12944d6834713e
Lopes <klinsmann.lopes@somapay.com.br>
2021-05-24 14:35:13 -0300

    Adding type of errors example

---
+++ src/main/java/com/br/currencyConverter/enums/BusinessRulesEnum.java
@@ -0,0 +1,11 @@
+package com.br.currencyConverter.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BusinessRulesEnum {
+    LIMIT_UNAVAILABLE,
+    SAQEXTRA_UNAVAILABLE;
+}
