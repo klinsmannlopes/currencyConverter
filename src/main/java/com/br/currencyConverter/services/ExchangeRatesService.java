db1752f4b5e2a5b454f7a9a6199ab3c13417cb1d
Lopes <klinsmann.lopes@somapay.com.br>
2021-05-24 16:37:26 -0300

    Throwing error when a

--- src/main/java/com/br/currencyConverter/services/ExchangeRatesService.java
+++ src/main/java/com/br/currencyConverter/services/ExchangeRatesService.java
@@ -1,21 +1,33 @@
 package com.br.currencyConverter.services;
 
-import com.br.currencyConverter.apis.exchangerate.ExchangeRatesApi;
-import com.br.currencyConverter.dtos.outputs.RateDTO;
+import com.br.currencyConverter.dtos.inputs.TransactionInputDTO;
+import com.br.currencyConverter.enums.BusinessRulesEnum;
 import com.br.currencyConverter.exceptions.BusinessRuleException;
+import com.br.currencyConverter.factory.ConverterFactory;
+import com.br.currencyConverter.interfaces.ConverterInterface;
+import com.br.currencyConverter.models.transactions.Transactions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import javax.management.InstanceNotFoundException;
 
 @Service
 public class ExchangeRatesService {
 
     @Autowired
-    private ExchangeRatesApi exchangeRatesApi;
+    private ConverterFactory converterFactory;
 
-    public RateDTO getListRates() throws BusinessRuleException {
-        RateDTO rateDTO = exchangeRatesApi.getListRates("110ed70748e8b3174324927a9d226b44", "BRL,USD,EUR,JPY");
-        return rateDTO;
+    public Mono<Transactions> converter(TransactionInputDTO transactionInputDTO) throws BusinessRuleException {
+        try {
+            ConverterInterface converterInterface = converterFactory.getInstance(transactionInputDTO.getDestinyCurrency());
+            Mono<Transactions> transaction = converterInterface.converterAndSave(transactionInputDTO);
+
+            return transaction;
+        } catch (InstanceNotFoundException e) {
+            throw new BusinessRuleException(BusinessRulesEnum.ERROR_CONVERTER, getClass(), "Error ao converter moedas");
+        }
+
     }
-
 
 }
