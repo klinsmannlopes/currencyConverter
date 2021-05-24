b8b860f45d01d5252a2ef02cf2429e753a9c5933
Lopes <klinsmann.lopes@somapay.com.br>
2021-05-24 20:03:39 -0300

    Correction text

--- src/main/java/com/br/currencyConverter/exceptions/CustomAttributes.java
+++ src/main/java/com/br/currencyConverter/exceptions/CustomAttributes.java
@@ -2,11 +2,12 @@ package com.br.currencyConverter.exceptions;
 
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.util.Map;
 
+@Component
 public class CustomAttributes extends DefaultErrorAttributes {
 
     @Override
@@ -16,7 +17,7 @@ public class CustomAttributes extends DefaultErrorAttributes {
         if (throwable instanceof ResponseStatusException) {
             ResponseStatusException ex = (ResponseStatusException) throwable;
             errorAttributesMap.put("message", ex.getMessage());
-            errorAttributesMap.put("developerMessage", "A ResponseStatusException Happened");
+            errorAttributesMap.put("developerMessage", "Entre em contato com o dev");
             return errorAttributesMap;
         }
 
