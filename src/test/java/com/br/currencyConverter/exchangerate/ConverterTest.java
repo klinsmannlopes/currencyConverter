f947208108e445b7bbfca4649140b258c8407fb4
Lopes <klinsmann.lopes@somapay.com.br>
2021-05-24 16:53:06 -0300

    Test by checking other currency conversions

--- src/test/java/com/br/currencyConverter/exchangerate/ConverterTest.java
+++ src/test/java/com/br/currencyConverter/exchangerate/ConverterTest.java
@@ -1,7 +1,6 @@
 package com.br.currencyConverter.exchangerate;
 
 import com.br.currencyConverter.enums.Currencies;
-import com.br.currencyConverter.exceptions.BusinessRuleException;
 import com.br.currencyConverter.factory.ConverterFactory;
 import com.br.currencyConverter.interfaces.ConverterInterface;
 import org.junit.jupiter.api.DisplayName;
@@ -33,6 +32,36 @@ public class ConverterTest {
         ConverterInterface converterInterface = getInterfaceImplTest(Currencies.BRL);
         double result = converterInterface.converter(rateBRL, euro);
         assertEquals(77.995464, result);
+    }
+
+    @Test
+    @DisplayName("Check from euro to dolar")
+    public void checkConverterUSDTest() throws InstanceNotFoundException {
+        double rateUSD = 1.221068;
+        double euro = 12;
+        ConverterInterface converterInterface = getInterfaceImplTest(Currencies.USD);
+        double result = converterInterface.converter(rateUSD, euro);
+        assertEquals(14.652816000000001, result);
+    }
+
+    @Test
+    @DisplayName("Check from euro to euro")
+    public void checkConverterEURTest() throws InstanceNotFoundException {
+        double rateEUR = 1;
+        double euro = 12;
+        ConverterInterface converterInterface = getInterfaceImplTest(Currencies.EUR);
+        double result = converterInterface.converter(rateEUR, euro);
+        assertEquals(12.0, result);
+    }
+
+    @Test
+    @DisplayName("Check from euro to yenes")
+    public void checkConverterJPYTest() throws InstanceNotFoundException {
+        double rateJPY = 132.835743;
+        double euro = 12;
+        ConverterInterface converterInterface = getInterfaceImplTest(Currencies.EUR);
+        double result = converterInterface.converter(rateJPY, euro);
+        assertEquals(1594.0289160000002, result);
     }
 
 }
